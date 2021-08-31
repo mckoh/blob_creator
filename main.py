09ef8aa0d83af7faa1d64fa2c5b4f5648e52593a
Michael Kohlegger MLM <michael.kohlegger@fh-kufstein.ac.at>
2021-08-31 18:08:13 +0200

    added cli readiness

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
-# %%
+from sys import argv
 from blob_creator.artist import Artist
 
-a = Artist(n=20, scatter=12, export_png=True)
-a.create_blobs()
-a.export_data()
+if __name__ == "__main__":
 
+    assert len(argv)==4, ValueError("To few arguments passed. Make sure you provide n, scatter and cols.")
+
+    n = int(argv[1])
+    s = int(argv[2])
+    c = int(argv[3])
+
+    a = Artist(n=n, scatter=s, cols=c, export_png=False)
+    a.create_blobs()
+    a.export_data()
