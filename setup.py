4a156d6d70013633d741285a3f8537512c5f3ea8
mckoh <59373584+mckoh@users.noreply.github.com>
2021-09-01 09:45:32 +0200

    created pypi package setup

---
+++ setup.py
@@ -0,0 +1,36 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='blob_creator',  
+    version='1.0',
+    author="Michael Kohlegger",
+    author_email="michael@datenberge.org",
+    description="Package to create dummy datasets for analysis tasks",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url = "https://github.com/mckoh/blob_creator",
+    project_urls={
+        "Bug Tracker": "https://github.com/mckoh/blob_creator/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+    install_requires=[
+        "openpyxl",
+        "pandas",
+        "numpy",
+        "matplotlib",
+        "Pillow",
+        "names",
+        "svglib",
+        "reportlab"
+    ]
+ )
