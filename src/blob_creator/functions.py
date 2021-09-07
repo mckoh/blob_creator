6f10df99ecbd933ee34e305cdfb9e68330620abb
Michael Kohlegger MLM <michael.kohlegger@fh-kufstein.ac.at>
2021-09-07 19:05:11 +0200

    changed from list to dict comprehension

--- src/blob_creator/functions.py
+++ src/blob_creator/functions.py
@@ -7,9 +7,9 @@ Date: 2021-09
 
 def dictionary_filter(dictionary, cols) -> dict:
     """Can be used to filter certain keys from a dict
-    
+
     :param dictionary: The original dictionary
     :param cols: The searched columns
     :return: A filtered dictionary
     """
-    return dict([(i, dictionary[i]) for i in dictionary if i in set(cols)])
+    return {key:value for (key,value) in dictionary.items() if key in cols}
