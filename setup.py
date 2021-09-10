2f33564b90cf4f3b2b9c171e8de4fae69da8bf10
mckoh <59373584+mckoh@users.noreply.github.com>
2021-09-10 07:39:06 +0200

    moved requirements solely to txt

--- setup.py
+++ setup.py
@@ -6,10 +6,12 @@ Date: 2021-09
 """
 
 import setuptools
-from src.blob_creator.requirements import hard_requirements
 
-with open("README.md", "r", encoding="utf8") as fh:
-    long_description = fh.read()
+with open("README.md", "r", encoding="utf8") as readme_file:
+    readme = readme_file.read()
+
+with open('requirements.txt', "r", encoding="utf8") as requirement_file:
+    requirements = requirement_file.read().splitlines()
 
 setuptools.setup(
     name='blob_creator',
@@ -17,7 +19,7 @@ setuptools.setup(
     author="Michael Kohlegger",
     author_email="michael@datenberge.org",
     description="Package to create dummy datasets for analysis tasks",
-    long_description=long_description,
+    long_description=readme,
     long_description_content_type="text/markdown",
     url = "https://github.com/mckoh/blob_creator",
     project_urls={
@@ -31,5 +33,5 @@ setuptools.setup(
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src"),
     python_requires=">=3.6",
-    install_requires=hard_requirements
+    install_requires=requirements
  )
